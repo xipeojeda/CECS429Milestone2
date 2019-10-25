88843f53e6cdedd0991cbc289661f28a1c3223be
Smushma <rollingrains@outlook.com>
2019-10-25 11:40:37 -0700

    Update Normalize.java
    This version of Normalize was used in the demo of Milestone1. This makes it so that our test cases pass 100% for "https://snowballstem.org/demo.html" and "google.com".  The previous version failed these two test cases.

--- src/cecs429/text/Normalize.java
+++ src/cecs429/text/Normalize.java
@@ -1,57 +1,53 @@
 package cecs429.text;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Scanner;
-import java.lang.Character;
-import cecs429.stemmer.*;
+import cecs429.stemmer.englishStemmer;
+
+import java.util.*;
 
 public class Normalize implements TokenProcessor
 {
-	public Normalize()
-	{
-		
+	private String language;
+
+	public Normalize(String language) {
+		this.language = language;
 	}
 
 	public List<String> processToken(String token) {
-		String newtokes=token;
+		String newTokens = token;
 		List<String> tokens = new ArrayList();// Set a token list to be returned later
-		 englishStemmer stemmer= new englishStemmer();
-		 String stemmed = null;
-		if(!Character.isLetterOrDigit(token.charAt(0))||!Character.isLetterOrDigit(token.charAt(token.length()-1)))
-		{//Block checks whether a token has a non alphanumeric at the beginning or end of the token and removes them
-		newtokes=token.replaceAll("\\W", "");
+		englishStemmer stemmer= new englishStemmer();
+
+		//Block checks whether a token has a non alphanumeric at the beginning or end of the token and removes them
+		if (!Character.isLetterOrDigit(token.charAt(0)) || !Character.isLetterOrDigit(token.charAt(token.length()-1)))  {
+			newTokens = token.replaceAll("(^[\\W_]*)|([\\W_]*$)", "");
 		}
-		if(token.contains("'")||tokens.contains("\""))
-		{
+		//System.out.println(newTokens);
+
 		//Block removes the quotation marks and single quotes of the token
-		newtokes=newtokes.replaceAll("'","");
-		newtokes=newtokes.replaceAll("\"","");
-		}
-		if(token.contains("-"))// Removes the hyphens
-			{
-				String tempstr = newtokes;
-			String[] temp = null;
-			temp=tempstr.split("[,?.-]+");
-		
-			List<String> temp2 = new ArrayList<String>(Arrays.asList(temp));
-			tokens = new ArrayList<String>(temp2);
-			newtokes=newtokes.replaceAll("\\W", "");//Needed so the final token doesn't contain an non alphanumeric
-			newtokes=newtokes.replaceAll("-","");
-			}
-		tokens.add(newtokes);//Adds the combined hyphen token and adds it to list
-		for(int i=0;i<tokens.size();i++)
-		{//Block that lowercases and stems.
-			tokens.set(i,tokens.get(i).toLowerCase());
-			stemmer.setCurrent(tokens.get(i));
-			stemmer.stem();
-			tokens.set(i,stemmer.getCurrent());
-		}
-		return tokens;
+		if (token.contains("'") || tokens.contains("\"")) {
+			newTokens = newTokens.replaceAll("'","");
+			newTokens = newTokens.replaceAll("\"","");
 		}
 
+		if (token.contains("-")) { //Removes the hyphens
+			String[] temp = null;
+			temp = token.split("[,?.-]+");
+
+			List<String> temp2 = new ArrayList<String>(Arrays.asList(temp));
+			tokens = new ArrayList<String>(temp2);
+			newTokens = newTokens.replaceAll("\\W", "");//Needed so the final token doesn't contain an non alphanumeric
+			newTokens = newTokens.replaceAll("-","");
+		}
+		tokens.add(newTokens); //Adds the combined hyphen token and adds it to list
+
+		for (int i=0;i < tokens.size(); i++) {//Block that lowercases and stems.
+			tokens.set(i, tokens.get(i).toLowerCase());
+			stemmer.setCurrent(tokens.get(i));
+			stemmer.stem();
+			tokens.set(i, stemmer.getCurrent());
+		}
+		//System.out.println(tokens);
+		return tokens;
+	}
 }
+
