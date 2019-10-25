cecd1ffe21ef9ee7f1a3d0873bf1fb358975139a
Smushma <rollingrains@outlook.com>
2019-10-25 12:37:22 -0700

    Normalize.java supports multiple languages
    Updated constructor to take in String param denoting language choice.  Language param will be used to select appropriate algorithm in switch statement.

--- src/cecs429/text/Normalize.java
+++ src/cecs429/text/Normalize.java
@@ -1,6 +1,7 @@
 package cecs429.text;
 
 import cecs429.stemmer.englishStemmer;
+import cecs429.stemmer.frenchStemmer;
 
 import java.util.*;
 
@@ -9,19 +10,18 @@ public class Normalize implements TokenProcessor
 	private String language;
 
 	public Normalize(String language) {
-		this.language = language;
+		this.language = language.toLowerCase();
 	}
 
 	public List<String> processToken(String token) {
 		String newTokens = token;
 		List<String> tokens = new ArrayList();// Set a token list to be returned later
-		englishStemmer stemmer= new englishStemmer();
 
 		//Block checks whether a token has a non alphanumeric at the beginning or end of the token and removes them
 		if (!Character.isLetterOrDigit(token.charAt(0)) || !Character.isLetterOrDigit(token.charAt(token.length()-1)))  {
 			newTokens = token.replaceAll("(^[\\W_]*)|([\\W_]*$)", "");
 		}
-		//System.out.println(newTokens);
+		//System.out.println(newTokens); //DEBUGGING
 
 		//Block removes the quotation marks and single quotes of the token
 		if (token.contains("'") || tokens.contains("\"")) {
@@ -40,13 +40,32 @@ public class Normalize implements TokenProcessor
 		}
 		tokens.add(newTokens); //Adds the combined hyphen token and adds it to list
 
-		for (int i=0;i < tokens.size(); i++) {//Block that lowercases and stems.
-			tokens.set(i, tokens.get(i).toLowerCase());
-			stemmer.setCurrent(tokens.get(i));
-			stemmer.stem();
-			tokens.set(i, stemmer.getCurrent());
+		/*
+			Process each token with the correct language stemmer
+		 */
+		switch (this.language) {
+			case "en":
+				englishStemmer enStemmer= new englishStemmer();
+
+				for (int i = 0; i < tokens.size(); i++) {//Block that lowercases and stems.
+					tokens.set(i, tokens.get(i).toLowerCase());
+					enStemmer.setCurrent(tokens.get(i));
+					enStemmer.stem();
+					tokens.set(i, enStemmer.getCurrent());
+				}
+				break;
+			case "fr":
+				frenchStemmer frStemmer= new frenchStemmer();
+
+				for (int i = 0; i < tokens.size(); i++) {//Block that lowercases and stems.
+					tokens.set(i, tokens.get(i).toLowerCase());
+					frStemmer.setCurrent(tokens.get(i));
+					frStemmer.stem();
+					tokens.set(i, frStemmer.getCurrent());
+				}
+				break;
 		}
-		//System.out.println(tokens);
+		//System.out.println(tokens); //DEBUGGING
 		return tokens;
 	}
 }
