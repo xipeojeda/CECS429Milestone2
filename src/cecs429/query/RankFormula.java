02f2ffb5a86c2960b07d0c2245d208b29649b727
Irvin Flores <irvinf413@gmail.com>
2019-11-10 14:55:54 -0800

    Update

--- src/cecs429/query/RankFormula.java
+++ src/cecs429/query/RankFormula.java
@@ -6,4 +6,5 @@ public interface RankFormula {
     double getWqt(Index i, String term);
     double getWdt(Index i, String term, int docId);
     double getLd(int docId);
+
 }
--- src/cecs429/query/RankedRetrieval.java
+++ src/cecs429/query/RankedRetrieval.java
@@ -1,13 +1,40 @@
 package cecs429.query;
 
 import cecs429.index.DiskIndexWriter;
+import cecs429.index.Index;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
-public class RankedRetrieval implements Ranking {
+public class RankedRetrieval implements Ranking, RankFormula {
 
     @Override
     public ArrayList<Accumalator> rankAlgorithm(String query, DiskIndexWriter index) {
+        HashMap<Integer, Double> accMap = new HashMap<>();
+        ArrayList<Accumalator> results = new ArrayList<>();
+        String[] tokens = query.split(" ");
+        for (int i =0; i < tokens; i++)
+        {
+            ArrayList<Positional>
+        }
+
         return null;
     }
+
+    @Override
+    public double getWqt(Index i, String term) {
+        return 0;
+    }
+
+    @Override
+    public double getWdt(Index i, String term, int docId) {
+        return 0;
+    }
+
+    @Override
+    public double getLd(int docId) {
+        return 0;
+    }
+
 }
