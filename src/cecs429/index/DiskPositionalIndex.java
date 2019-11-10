e2d0c9176617373a89572b7dcd30eed13f2ad7fa
Jesus <jesusojedapvcs@gmail.com>
2019-11-10 14:56:26 -0800

    Update DiskPositionalIndex.java

--- src/cecs429/index/DiskPositionalIndex.java
+++ src/cecs429/index/DiskPositionalIndex.java
@@ -11,6 +11,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,29 +28,75 @@ public class DiskPositionalIndex implements Index {
 	private String mPath;
 	private RandomAccessFile mVocabList;
 	private RandomAccessFile mPostings;
+	private RandomAccessFile docWeights;
 	private long[] mVocabTable;
-	private ConcurrentNavigableMap<String, Long> postingsDB;
+	private List<String> mFileNames;
+	private List<String> terms = null;
+	private ConcurrentNavigableMap<String, Long> vocabDB;
 	private DB db;
 	
 	
 	public DiskPositionalIndex(String path) {
 		try {
+			
 			mPath = path;
 			mVocabList = new RandomAccessFile(new File(path, "vocab.txt"), "r");
 			mPostings = new RandomAccessFile(new File(path, "postings.bin"), "r");
-			this.postingsDB = this.db.treeMap("vocabTree", Serializer.STRING, Serializer.LONG).open();
+			vocabDB = this.db.treeMap("vocabTree", Serializer.STRING, Serializer.LONG).open();
+			mVocabTable = readVocabTable(path);
+			mFileNames = readFileNames(path);
+			docWeights = new RandomAccessFile(new File(path, "docWeights.bin"), "r");
 		}catch(FileNotFoundException e) {
 			System.out.println(e.toString());
 		}
 	}
-    @Override
-    public List<Posting> getPostings(String term) {
-    		long position = postingsDB.get(term);
+
+	public List<Posting> getPostings(String term, boolean positions) {
+    		long position = bTreeSearchVocab(term);
     		if(position >= 0) {
     			return readPostingsBin(mPostings, position);
     		}
         	return null;
     }
+	
+	private long bTreeSearchVocab(String term){
+		long vListPosition = vocabDB.get(term);
+	      // do a binary search over the vocabulary, using the vocabTable and the file vocabList.
+	      int i = 0, j = mVocabTable.length / 2 - 1;
+	      while (i <= j) {
+	         try {
+	        	  int m = (i + j) / 2;
+	        	  int termLength;
+	             if (m == mVocabTable.length / 2 - 1) {
+	                 termLength = (int)(mVocabList.length() - mVocabTable[m*2]);
+	              }
+	              else {
+	                 termLength = (int) (mVocabTable[(m + 1) * 2] - vListPosition);
+	              }
+	            mVocabList.seek(vListPosition);
+
+	            byte[] buffer = new byte[termLength];
+	            mVocabList.read(buffer, 0, termLength);
+	            String fileTerm = new String(buffer, "ASCII");
+
+	            int compareValue = term.compareTo(fileTerm);
+	            if (compareValue == 0) {
+	               // found it!
+	               return mVocabTable[m * 2 + 1];
+	            }
+	            else if (compareValue < 0) {
+	               j = m - 1;
+	            }
+	            else {
+	               i = m + 1;
+	            }
+	         }
+	         catch (IOException ex) {
+	            System.out.println(ex.toString());
+	         }
+	      }
+	      return -1;
+	}
     
     public List<Posting> readPostingsBin(RandomAccessFile postings, long pos){
    
@@ -55,7 +107,7 @@ public class DiskPositionalIndex implements Index {
     		//seek to the position where postings start
 			postings.seek(pos);
 			
-			// read the 4 bytes for doc freq
+			// read the 8 bytes for doc freq
 			byte[] buffer = new byte[8];
 			postings.read(buffer, 0, buffer.length);
 			
@@ -114,14 +166,14 @@ public class DiskPositionalIndex implements Index {
 			long[] vocabTable;
 			RandomAccessFile tableFile = new RandomAccessFile(new File(indexName, "vocabTable.bin"), "r");
 			
-			byte[] byteBuffer = new byte[4];
+			byte[] byteBuffer = new byte[8];
 			tableFile.read(byteBuffer, 0, byteBuffer.length);
 			
 			int tableIndex = 0;
 			vocabTable = new long[ByteBuffer.wrap(byteBuffer).getInt() * 2];
 			byteBuffer = new byte[8];
-			//while we keep reading 4 bytes
-			while(tableFile.read(byteBuffer) > 0) {
+			//while we keep reading 8 bytes
+			while(tableFile.read(byteBuffer,0,byteBuffer.length) > 0) {
 				vocabTable[tableIndex] = ByteBuffer.wrap(byteBuffer).getLong();
 				tableIndex++;
 			}
@@ -139,5 +191,79 @@ public class DiskPositionalIndex implements Index {
 	
 	public int getTermCount() {
 		return mVocabTable.length / 2;
+	}
+	/*
+	 * Walk file tree to get file names
+	 * @param path Directory path
+	 * @return arraylist of file names
+	 */
+    private List<String> readFileNames(String path) {
+		// TODO Auto-generated method stub
+		List<String> fileNames = new ArrayList<>();
+		try {
+			Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>(){
+				@Override
+				public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+					return FileVisitResult.CONTINUE;
+				}
+				
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws FileNotFoundException{
+					//only process json files
+					if(file.toString().endsWith(".json")) {
+						fileNames.add(file.toFile().getName());//add to list
+					}
+					return FileVisitResult.CONTINUE;
+				}
+				// We dont want to throw exceptions if files are locked
+				//or other errors occur
+				@Override
+				public FileVisitResult visitFileFailed(Path file, IOException e) {
+					return FileVisitResult.CONTINUE;
+				}
+			});
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return fileNames;
+	}
+	
+	public List<String> getFileNames(){
+		return mFileNames;
+	}
+	
+	public String[] getVocab() {
+		List<String> vocabList = new ArrayList<>();
+		int i = 0;
+		int j = mVocabTable.length /2 - 1;
+		while(i <= j) {
+			try {
+				
+			
+			int termLength;
+			if(i == j) {
+				termLength = (int) (mVocabList.length() - mVocabTable[i * 2]);
+			}
+			else {
+				termLength = (int) (mVocabList.length() - mVocabTable[i * 2]);
+			}
+			
+			byte[] buffer = new byte[termLength];
+			mVocabList.read(buffer, 0, termLength);
+			String term = new String(buffer, "ASCII");
+			vocabList.add(term);
+			}catch(IOException e) {
+				e.printStackTrace();
+			i++;
+			}
+		}
+		return vocabList.toArray(new String[0]);
+	}
+
+	@Override
+	public List<Posting> getPostings(String term) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
